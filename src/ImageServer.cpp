79d7e253fdc2b6d3f9925cce09608f4f772f978f
guy <guy@guy-laptop-ubuntu>
2012-11-12 18:37:31 -0500

    test commit from eclipse

--- src/ImageServer.cpp
+++ src/ImageServer.cpp
@@ -7,7 +7,7 @@
 void *PrintHello(void *threadid) {
 	long tid;
 	tid = (long) threadid;
-	printf("Hello World! It's me, thread #%ld!\n", tid);
+	printf("Hello World! It's me, thread : #%ld!\n", tid);
 	pthread_exit(NULL);
 }
 
