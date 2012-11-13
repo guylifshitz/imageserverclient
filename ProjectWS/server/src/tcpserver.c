2db1f23c38fed39fb8e7d3dc182534f21c3e8ed8
Guy Lifshitz <guylifshitz@gmail.com>
2012-11-13 01:09:30 -0500

    Client stuff

---
+++ ProjectWS/server/src/tcpserver.c
@@ -0,0 +1,66 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <errno.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "listen_server.h"
+
+const char *DAYTIME_PORT="13111";
+
+int
+main(int argc, char *argv[])
+{
+    int listenfd, connfd;
+    socklen_t addrlen;
+    char timeStr[256];
+    struct sockaddr_storage clientaddr;
+    time_t now;
+    char clienthost[NI_MAXHOST];
+    char clientservice[NI_MAXSERV];
+
+    /* local server socket listening at daytime port=13 */
+    listenfd = listen_server(NULL, DAYTIME_PORT, 
+                             AF_UNSPEC, SOCK_STREAM);
+
+    if (listenfd < 0) {
+         fprintf(stderr,
+                 "listen_socket error:: could not create listening "
+                 "socket\n");
+         return -1;
+    }
+
+    for ( ; ;) {
+        addrlen = sizeof(clientaddr);
+
+        /* accept daytime client connections */
+        connfd = accept(listenfd, 
+                        (struct sockaddr *)&clientaddr, 
+                        &addrlen);
+
+        if (connfd < 0) 
+            continue;
+
+        memset(clienthost, 0, sizeof(clienthost));
+        memset(clientservice, 0, sizeof(clientservice));
+
+        getnameinfo((struct sockaddr *)&clientaddr, addrlen,
+                    clienthost, sizeof(clienthost),
+                    clientservice, sizeof(clientservice),
+                    NI_NUMERICHOST);
+
+        printf("Received request from host=[%s] port=[%s]\n",
+               clienthost, clientservice);
+
+        /* process daytime request from a client */
+        memset(timeStr, 0, sizeof(timeStr));
+        time(&now);
+        sprintf(timeStr, "%s", ctime(&now));
+        write(connfd, timeStr, strlen(timeStr));
+        close(connfd);
+    }
+
+    return 0;
+}
